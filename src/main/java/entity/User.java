9b4f291a22017deba6f12e02f361e47b1555cdf0
Jagoda_Jarzebowska <jagoda.jarz@gmail.com>
2018-08-05 19:39:51 +0200

    added first entity 'user'

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,29 @@
+package entity;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String name;
+
+    @NonNull
+    @Column(nullable = false)
+    private String email;
+
+}
